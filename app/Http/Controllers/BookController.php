3fcd350768b9ea5ce1da4ee7436416ee043f7720
Rafael Brasil <rafaelpontesbr@gmail.com>
2019-04-30 14:59:12 -0400

    feature(wip): crud book
    Quase finalizado o crud dos livros faltando somente a parte do upload de
    imagem.

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+
+        return view('book.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('book.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|unique:books|max:255',
+            'description' => 'required'
+        ]);
+
+        Book::create($validatedData);
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::FindOrFail($id);
+
+        return view('book.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $book = Book::FindOrFail($id);
+
+        return view('book.create', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::FindOrFail($id);
+
+        $validatedData = $request->validate([
+            'title' => 'required|unique:books|max:255',
+            'description' => 'required'
+        ]);
+
+        $book->fill($validatedData);
+
+        if ($book->save()) {
+            return redirect()->route('books.index');
+        }
+
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::FindOrFail($id);
+
+        if ($book->destroy()) {
+            return redirect()->route('books.index');
+        }
+
+        return redirect()->route('books.index');
+    }
+}
