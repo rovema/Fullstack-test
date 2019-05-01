1b5f10b08ccd7ee2f90bc83bd10cd7116659f30b
Rafael Brasil <rafaelpontesbr@gmail.com>
2019-05-01 00:15:02 -0400

    wip: crud BookCase

---
+++ app/Http/Controllers/BookCaseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BookCase;
+use Illuminate\Http\Request;
+
+class BookCaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BookCase  $bookCase
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookCase $bookCase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BookCase  $bookCase
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookCase $bookCase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BookCase  $bookCase
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookCase $bookCase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BookCase  $bookCase
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookCase $bookCase)
+    {
+        //
+    }
+}
