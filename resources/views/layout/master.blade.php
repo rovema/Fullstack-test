7f7cf6d7df3852e473149d324ecef85aea29c822
Rafael Brasil <rafaelpontesbr@gmail.com>
2019-05-01 19:50:58 -0400

    fix: alterado metodo mix para asset

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -15,7 +15,7 @@
     <link href="{{ asset('css/base.css') }}" rel="stylesheet">
 
     {{-- Scripts --}}
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     @include('layout.navbar')
