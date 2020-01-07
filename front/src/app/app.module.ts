4f0b2039fcd51c1bc27971fd24017d7e7ea2c66f
Magno Carvalho <magnocarv@hotmail.com>
2020-01-07 19:44:40 -0400

    singup component create

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { LoginComponent } from "./page/login/login.component";
 import { MainComponent } from "./page/main/main.component";
 import { MaterialAllStyle } from "./material.modules";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { SingupComponent } from './page/singup/singup.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, MainComponent],
+  declarations: [AppComponent, LoginComponent, MainComponent, SingupComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
