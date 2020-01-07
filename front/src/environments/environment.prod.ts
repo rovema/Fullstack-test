7688437bec0ba1a31337ad1ee8c9b6c74e5fa777
Magno Carvalho <magnocarv@hotmail.com>
2020-01-07 16:02:57 -0400

    firebase config e credential

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBOJ_F1wvRGa40_0bTDCWfEXLfPPP7810",
+    authDomain: "magno-test-rovema.firebaseapp.com",
+    databaseURL: "https://magno-test-rovema.firebaseio.com",
+    projectId: "magno-test-rovema",
+    storageBucket: "magno-test-rovema.appspot.com",
+    messagingSenderId: "870215955925",
+    appId: "1:870215955925:web:095074091a53e109f3795c",
+    measurementId: "G-W03ZMGVM5V"
+  }
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBOJ_F1wvRGa40_0bTDCWfEXLfPPP7810",
+    authDomain: "magno-test-rovema.firebaseapp.com",
+    databaseURL: "https://magno-test-rovema.firebaseio.com",
+    projectId: "magno-test-rovema",
+    storageBucket: "magno-test-rovema.appspot.com",
+    messagingSenderId: "870215955925",
+    appId: "1:870215955925:web:095074091a53e109f3795c",
+    measurementId: "G-W03ZMGVM5V"
+  }
 };
 
 /*
