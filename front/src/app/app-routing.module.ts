d36a1b7f34b82471282d840d3aada7c87d3f6f45
Magno Carvalho <magnocarv@hotmail.com>
2020-01-07 19:43:29 -0400

    package material design add and routes init

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./page/login/login.component";
+import { MainComponent } from "./page/main/main.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "auth", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PoModule } from '@portinari/portinari-ui';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PoModule } from "@portinari/portinari-ui";
+import { RouterModule } from "@angular/router";
+import { LoginComponent } from "./page/login/login.component";
+import { MainComponent } from "./page/main/main.component";
+import { MaterialAllStyle } from "./material.modules";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, MainComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     PoModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    MaterialAllStyle,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
