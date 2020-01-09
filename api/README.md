cd7d285bbea5d166b7f6cba2867aca82a363b191
Magno Carvalho <magnocarv@hotmail.com>
2020-01-09 02:12:46 -0400

    init api index.js file

---
+++ api/README.md
@@ -0,0 +1,4 @@
+Api Node do desafio rovema full stack
+npm i -g gulp
+npm install build-tools  -g
+
---
+++ api/src/index.ts
@@ -0,0 +1,86 @@
+import rota = require("./routes/rotas");
+import express = require("express");
+import mongoose = require("mongoose");
+import bodyParser = require("body-parser");
+import moment = require("moment");
+import admin = require("firebase-admin");
+
+var cors = require("cors");
+var app = express();
+require("dotenv").config();
+
+moment.locale("pt-BR");
+
+app.use(cors());
+app.options("*", cors());
+
+app.use(bodyParser.json({ limit: "1mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false); // biblioteca depreciada
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log(
+      "🚀 Mongo DB inicializado com sucesso as",
+      moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+    );
+  });
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+
+app.options("*", function(req, res, next) {
+  if (req.method == "OPTIONS") res.sendStatus(200);
+});
+app.use("/api", rota); //rotas api
+
+app.use(function(req, res, next) {
+  let err: any;
+  err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || "Something went wrong. Please try again",
+    status: err.status || 500
+  });
+});
+
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      status: err.status || 500
+    });
+  });
+}
+
+var port = process.env.PORT || 1337;
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: `https://oficina2utfpr.firebaseio.com`
+  });
+  console.log("🚀 Server app firebase iniciado");
+} catch (e) {
+  console.log("🚀 Server app firebase falhou : ", e);
+}
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}/api`);
+});
